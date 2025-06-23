9e311f2ee3c1ed9119dda39201bb473808f87240
Freddy Ramírez <furede87@gmail.com>
2025-06-23 12:21:59 -0600

    Create and spawn enemy entities

--- procgen.py
+++ procgen.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING, Iterator, List, Tuple
 import tcod
 
 import tile_types
+import entity_factories
 from game_map import GameMap
 
 if TYPE_CHECKING:
@@ -40,6 +41,19 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )
 
+def place_entities(
+   room: RectangularRoom, dungeon: GameMap, maximum_monsters: int,
+) -> None:
+   number_of_monsters = random.randint(0, maximum_monsters)
+   for i in range(number_of_monsters):
+       x = random.randint(room.x1 + 1, room.x2 - 1)
+       y = random.randint(room.y1 + 1, room.y2 - 1)
+       if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+           if random.random() < 0.8:
+               entity_factories.orc.spawn(dungeon, x, y)
+           else:
+               entity_factories.troll.spawn(dungeon, x, y)
+
 
 def tunnel_between(
     start: Tuple[int, int], end: Tuple[int, int]
@@ -69,6 +83,7 @@ def generate_dungeon(
     room_max_size: int,
     map_width: int,
     map_height: int,
+    max_monsters_per_room: int,
     player: Entity,
 ) -> GameMap:
     """Generate a new dungeon map."""
@@ -102,6 +117,8 @@ def generate_dungeon(
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
 
+        
+        place_entities(new_room, dungeon, max_monsters_per_room)
         # Finally, append the new room to the list.
         rooms.append(new_room)
 
